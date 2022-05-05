b3aa89f7ac0613c3682815ee8e88fe937f8298ad
Callum Hopkins <103919468@student.swin.edu.au>
2022-05-05 16:39:03 +1000

    Completed 2

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function helloWorld()
 {
     document.getElementById('helloWorld').innerHTML = "Hello World"
 }
+
+function two()
+{
+    document.getElementById('helloWorld').innerHTML = "Goodbye Mars"
+}
